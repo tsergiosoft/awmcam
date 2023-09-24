bec705f80728d4e6430c9429aeec2f7f6b1c48e0
tsergiosoft <sergtarasenko76@gmail.com>
2023-09-24 09:55:47 -0700

    commit

--- service/awmrun.sh
+++ service/awmrun.sh
@@ -4,7 +4,7 @@
 #screen -S cam -d -m bash -c "/home/pi/awm/cam_run.sh"
 #screen -S sshmav -d -m bash -c "/home/pi/awm/ssh_mav.sh"
 screen -dmS dk python3 /home/pi/awmcam/dk.py
-screen -dmS pystream python3 /home/pi/awmcam/pystream.py
+#screen -dmS pystream python3 /home/pi/awmcam/pystream.py
 
 ##### temp - call from dk!!!
 screen -S sshcam -d -m bash -c "/home/pi/awmcam/ssh/ssh_cam.sh"
