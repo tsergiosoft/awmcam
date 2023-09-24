b883037080aa6e4401d1385a3ac5cabc6feaf55f
tsergiosoft <sergtarasenko76@gmail.com>
2023-09-24 09:27:46 -0700

    commit

--- service/awmrun.sh
+++ service/awmrun.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-screen -S ssh22 -d -m bash -c "/home/pi/awm/ssh_22.sh"
+screen -S ssh22 -d -m bash -c "/home/pi/awmcam/ssh_22.sh"
 
-#screen -S mavproxy -d -m bash -c "/home/pi/awm/mavproxy_run.sh"
-#screen -S cam -d -m bash -c "/home/pi/awm/cam_run.sh"
-#screen -S sshcam -d -m bash -c "/home/pi/awm/ssh_cam.sh"
-#screen -S sshmav -d -m bash -c "/home/pi/awm/ssh_mav.sh"
 
-#screen -S pystream -d -m bash -c "/home/pi/awmcam/service/pystream_run.sh"
-#screen -S pystream -d -m python3 /home/pi/awmcam/pystream.py
+#screen -S mavproxy -d -m bash -c "/home/pi/awmcam/mavproxy_run.sh"
+#screen -S cam -d -m bash -c "/home/pi/awmcam/cam_run.sh"
+#screen -S sshmav -d -m bash -c "/home/pi/awmcam/ssh_mav.sh"
+
 screen -dmS pystream python3 /home/pi/awmcam/pystream.py
+screen -S sshcam -d -m bash -c "/home/pi/awmcam/ssh_cam.sh"
+
 while true ; do sleep 120; done
 
