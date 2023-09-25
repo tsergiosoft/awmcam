33aea646830ee2d5a85268a9d356be3d2fe33563
tsergiosoft <sergtarasenko76@gmail.com>
2023-09-25 10:59:53 -0700

    commit

--- awm.py
+++ awm.py
@@ -3,13 +3,13 @@ from pymavlink import mavutil
 import time
 import configparser
 import os
+from webcam import webcamserver
 
 #import argparse  
 #parser = argparse.ArgumentParser()
 #parser.add_argument('--connect', default='127.0.0.1:5160')
-
 #args = parser.parse_args()
-      
+
 config = configparser.ConfigParser()
 config.read('params.ini')
 TALON_SN	=config['DEFAULT']['SN']
@@ -21,7 +21,17 @@ REMOTE_MAV_PORT =config['DEFAULT']['REMOTE_MAV_PORT']
 MAV_MASTER	=config['DEFAULT']['MAV_MASTER']
 MAV_BAUD	=config['DEFAULT']['MAV_BAUD']
 #MAVPROXY_IP_PORT=config['DEFAULT']['MAVPROXY_IP_PORT'] #may be delete and use 127.0.0.1:14550
-MAV_DRONEKIT	=config['DEFAULT']['MAV_DRONEKIT']
+MAV_DRONEKIT=config['DEFAULT']['MAV_DRONEKIT']
+PICAM	    =config['DEFAULT']['PICAM']
+
+PICAM=False;
+#pi=True;
+if (PICAM):
+    from picamera2 import Picamera2
+    from picamera2.encoders import JpegEncoder
+    from picamera2.outputs import FileOutput
+
+
 print("TALON_SN="+TALON_SN+" CLOUD_IP="+CLOUD_IP)
 
 #os.system('pkill screen')
@@ -30,7 +40,7 @@ print("TALON_SN="+TALON_SN+" CLOUD_IP="+CLOUD_IP)
 os.system('screen -S ssh22 -X kill')
 os.system('screen -S sshweb -X kill')
 os.system('screen -S sshmav -X kill')
-os.system('screen -S mavproxy -X kill')
+os.system('screen -S mav -X kill')
 os.system('screen -S web -X kill')
 time.sleep(2)
 os.system('screen -dmS ssh22 bash -c "/home/pi/awmcam/ssh_rev_tunnel.sh -cloud_ip='+CLOUD_IP+' -cloud_user='+CLOUD_USER+' -cloud_port='+REMOTE_SSH_PORT+' -local_port=22"')
@@ -39,6 +49,29 @@ os.system('screen -dmS sshmav bash -c "/home/pi/awmcam/ssh_rev_tunnel.sh -cloud_
 os.system('screen -dmS mav bash -c "/home/pi/awmcam/mavproxy.sh -m '+MAV_MASTER+' -p '+MAV_DRONEKIT+' -b '+MAV_BAUD+'"')
 #os.system('screen -dmS web bash -c "python3 /home/pi/awmcam/webhello.py --port 8080"')
 os.system('screen -dmS web bash -c "python3 /home/pi/awmcam/webcam.py --port 8080"')
+
+server = webcamserver('', 8080)
+server.start()
+
+# try:
+#     webcamserver.join()
+# except:
+#     pass
+
+# if (PICAM):
+#     picam2 = Picamera2()
+#     picam2.configure(picam2.create_video_configuration(main={"size": (800, 600)}))
+#     output = webcamserver.StreamingOutput()
+#     picam2.start_recording(JpegEncoder(), FileOutput(output))
+
+# try:
+#     address = ('', 8080)
+#     print("WEB PICAMERA HQ http://host:"+REMOTE_CAM_PORT)
+#     server = webcam.StreamingServer(address, webcam.StreamingHandler)
+#     server.serve_forever()
+# finally:
+#     picam2.stop_recording()
+
 
 LinkOK=False
 vehicle = connect(MAV_DRONEKIT,baud=MAV_BAUD, wait_ready=True, heartbeat_timeout=100,timeout=100)
@@ -57,47 +90,4 @@ while True:
     print(vehicle.parameters['AFS_ENABLE'])
     #print(vehicle.mode.name)
     
-print ('Connecting ON')
-# Function to arm and then takeoff to a user specified altitude
-def arm_and_takeoff(aTargetAltitude):
 
-  print("Basic pre-arm checks")
-  # Don't let the user try to arm until autopilot is ready
-  while not vehicle.is_armable:
-    print (" Waiting for vehicle to initialise...")
-    time.sleep(1)
-        
-  print("Arming motors")
-  # Copter should arm in GUIDED mode
-  vehicle.mode    = VehicleMode("GUIDED")
-  vehicle.armed   = True
-
-  while not vehicle.armed:
-    print(" Waiting for arming...")
-    time.sleep(1)
-
-  print("Taking off!")
-  vehicle.simple_takeoff(aTargetAltitude) # Take off to target altitude
-
-  # Check that vehicle has reached takeoff altitude
-  while True:
-    #print (" Altitude: ", vehicle.location.global_relative_frame.alt)
-    #Break and return from function just below target altitude.        
-    if vehicle.location.global_relative_frame.alt>=aTargetAltitude*0.95: 
-      print ("Reached target altitude")
-      break
-    time.sleep(1)
-
-# Initialize the takeoff sequence to 20m
-arm_and_takeoff(20)
-
-print("Take off complete")
-
-# Hover for 10 seconds
-time.sleep(10)
-
-print("Now let's land")
-vehicle.mode = VehicleMode("LAND")
-
-# Close vehicle object
-vehicle.close()
--- webcam.py
+++ webcam.py
@@ -9,11 +9,8 @@ import logging
 import socketserver
 from http import server
 from threading import Condition
-import argparse
 
-from picamera2 import Picamera2
-from picamera2.encoders import JpegEncoder
-from picamera2.outputs import FileOutput
+import threading
 
 PAGE = """\
 <html>
@@ -27,79 +24,83 @@ PAGE = """\
 </html>
 """
 
+class webcamserver(threading.Thread):
+    def __init__(self, host="localhost", port=8081, pycam=False):
+        super().__init__()
+        self.host = host
+        self.port = port
+        self.pycam = pycam
+        self.output = self.StreamingOutput()
 
-class StreamingOutput(io.BufferedIOBase):
-    def __init__(self):
-        self.frame = None
-        self.condition = Condition()
+    class StreamingOutput(io.BufferedIOBase):
+        def __init__(self):
+            self.frame = None
+            self.condition = Condition()
 
-    def write(self, buf):
-        with self.condition:
-            self.frame = buf
-            self.condition.notify_all()
+        def write(self, buf):
+            with self.condition:
+                self.frame = buf
+                self.condition.notify_all()
+
+    class StreamingHandler(server.BaseHTTPRequestHandler):
+        def __init__(self, output, *args):
+            self.output = output
+            super().__init__(*args)
+
+        def do_GET(self):
+            if self.path == '/':
+                self.send_response(301)
+                self.send_header('Location', '/index.html')
+                self.end_headers()
+            elif self.path == '/index.html':
+                content = PAGE.encode('utf-8')
+                self.send_response(200)
+                self.send_header('Content-Type', 'text/html')
+                self.send_header('Content-Length', len(content))
+                self.end_headers()
+                self.wfile.write(content)
+            elif self.path == '/stream.mjpg':
+                self.send_response(200)
+                self.send_header('Age', 0)
+                self.send_header('Cache-Control', 'no-cache, private')
+                self.send_header('Pragma', 'no-cache')
+                self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
+                self.end_headers()
+                try:
+                    while True:
+                        with self.output.condition:
+                            self.output.condition.wait()
+                            frame = self.output.frame
+                        self.wfile.write(b'--FRAME\r\n')
+                        self.send_header('Content-Type', 'image/jpeg')
+                        self.send_header('Content-Length', len(frame))
+                        self.end_headers()
+                        self.wfile.write(frame)
+                        self.wfile.write(b'\r\n')
+                except Exception as e:
+                    logging.warning(
+                        'Removed streaming client %s: %s',
+                        self.client_address, str(e))
+            else:
+                self.send_error(404)
+                self.end_headers()
+
+    class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
+        allow_reuse_address = True
+        daemon_threads = True
+
+    def run(self):
+        address = (self.host, self.port)
+        server = self.StreamingServer(address, lambda *args, **kwargs: self.StreamingHandler(self.output, *args))
+        server.serve_forever()
+    def start_stream(self):
+        if self.pycam:
+            picam2 = Picamera2()
+            picam2.configure(picam2.create_video_configuration(main={"size": (800, 600)}))
+            picam2.start_recording(JpegEncoder(), FileOutput(output))
+
+    def stop_stream(self):
+        if self.pycam:
+            picam2.stop_recording()
 
 
-class StreamingHandler(server.BaseHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == '/':
-            self.send_response(301)
-            self.send_header('Location', '/index.html')
-            self.end_headers()
-        elif self.path == '/index.html':
-            content = PAGE.encode('utf-8')
-            self.send_response(200)
-            self.send_header('Content-Type', 'text/html')
-            self.send_header('Content-Length', len(content))
-            self.end_headers()
-            self.wfile.write(content)
-        elif self.path == '/stream.mjpg':
-            self.send_response(200)
-            self.send_header('Age', 0)
-            self.send_header('Cache-Control', 'no-cache, private')
-            self.send_header('Pragma', 'no-cache')
-            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
-            self.end_headers()
-            try:
-                while True:
-                    with output.condition:
-                        output.condition.wait()
-                        frame = output.frame
-                    self.wfile.write(b'--FRAME\r\n')
-                    self.send_header('Content-Type', 'image/jpeg')
-                    self.send_header('Content-Length', len(frame))
-                    self.end_headers()
-                    self.wfile.write(frame)
-                    self.wfile.write(b'\r\n')
-            except Exception as e:
-                logging.warning(
-                    'Removed streaming client %s: %s',
-                    self.client_address, str(e))
-        else:
-            self.send_error(404)
-            self.end_headers()
-
-
-class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
-    allow_reuse_address = True
-    daemon_threads = True
-
-parser = argparse.ArgumentParser(description="Custom HTTP Server")
-parser.add_argument("--host", default="localhost", help="Host name to listen on (default: localhost)")
-parser.add_argument("--port", type=int, default=8080, help="Port number to listen on (default: 8080)")
-args = parser.parse_args()
-
-host = args.host
-port = args.port
-
-picam2 = Picamera2()
-picam2.configure(picam2.create_video_configuration(main={"size": (800, 600)}))
-output = StreamingOutput()
-picam2.start_recording(JpegEncoder(), FileOutput(output))
-
-try:
-    address = (host, port)
-    print("WEB PICAMERA HQ http://" + host + ":" + str(port))
-    server = StreamingServer(address, StreamingHandler)
-    server.serve_forever()
-finally:
-    picam2.stop_recording()
