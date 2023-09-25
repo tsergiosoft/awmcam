65153428f79f668c1be1dd81f1a838ed088288f0
tsergiosoft <sergtarasenko76@gmail.com>
2023-09-25 11:51:53 -0700

    commit

--- webcam.py
+++ webcam.py
@@ -106,8 +106,9 @@ class webcamserver(threading.Thread):
     def start_stream(self):
         if self.pycam:
             print("Start stream")
+            print(self.picam2.sensor_modes)
             #picam2 = Picamera2()
-            self.picam2.configure(picam2.create_video_configuration(main={"size": (800, 600)}))
+            self.picam2.configure(self.picam2.create_video_configuration(main={"size": (800, 600)}))
             self.picam2.start_recording(JpegEncoder(), FileOutput(self.output))
 
     def stop_stream(self):
