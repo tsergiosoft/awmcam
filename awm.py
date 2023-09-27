0506c710f12ea8f701e1b8d2e11e06e0afd88403
tsergiosoft <sergtarasenko76@gmail.com>
2023-09-27 02:06:56 -0700

    commit

--- awm.py
+++ awm.py
@@ -65,7 +65,7 @@ class cam():
     def start_file(self):
         pass
 
-os.system('mount -o uid=pi,gid=pi /dev/sda1 ~/usb/')
+os.system('sudo mount -o uid=pi,gid=pi /dev/sda1 ~/usb/')
 # wserver = webcamserver(host="localhost", port=8080)
 # wserver.start() #Thread
 
@@ -81,4 +81,4 @@ pcam.stop_stream()
 # print("stop wserver")
 # wserver.stop() #Thread
 # wserver.join()
-os.system('umount -o ~/usb/')
+os.system('sudo umount -o ~/usb/')
