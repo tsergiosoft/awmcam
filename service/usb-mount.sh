79d54ad6788fa35632dfa84de11a2a48bc347225
tsergiosoft <sergtarasenko76@gmail.com>
2023-09-30 02:54:24 -0700

    open cv

--- service/usb-mount.sh
+++ service/usb-mount.sh
@@ -10,12 +10,12 @@ case "${ACTION}" in
         eval $(/sbin/blkid -o udev ${DEVICE})                # Get info for this drive: $ID_FS_LABEL, $ID_FS_UUID, and $ID_FS_TYPE
         OPTS="rw,relatime"                                   # Global mount options
         if [[ ${ID_FS_TYPE} == "vfat" ]]; then OPTS+=",users,gid=100,umask=000,shortname=mixed,utf8=1,flush"; fi     # File system type specific mount options
-#        if ! /bin/mount -o ${OPTS} ${DEVICE} /media/; then exit 1; fi          # Error during mount process: cleanup mountpoint
-        if ! /bin/mount -o ${OPTS} /dev/sda1 /media/; then exit 1; fi          # Error during mount process: cleanup mountpoint
+        if ! /bin/mount -o ${OPTS} ${DEVICE} /media/; then exit 1; fi          # Error during mount process: cleanup mountpoint
+#        if ! /bin/mount -o ${OPTS} /dev/sda1 /media/; then exit 1; fi          # Error during mount process: cleanup mountpoint
         ;;
     remove)
-#        if [[ -n ${MOUNT_POINT} ]]; then /bin/umount -l ${DEVICE}; fi
-        if [[ -n ${MOUNT_POINT} ]]; then /bin/umount -l /dev/sda1; fi
+        if [[ -n ${MOUNT_POINT} ]]; then /bin/umount -l ${DEVICE}; fi
+#        if [[ -n ${MOUNT_POINT} ]]; then /bin/umount -l /dev/sda1; fi
         ;;
 esac
 
