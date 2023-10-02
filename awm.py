0da1de3275033a68ca49f330073bf86c0be5f0df
tsergiosoft <sergtarasenko76@gmail.com>
2023-10-02 02:55:25 -0700

    open cv

--- awm.py
+++ awm.py
@@ -76,7 +76,7 @@ while True:
     VID_TIME = int(vehicle.parameters['VID_TIME'])
     VID_WEB_MODE = int(vehicle.parameters['VID_WEB_MODE'])
     info2 = "VID_ON=%s VID_TIME=%s VID_WEB_MODE=%s" % (VID_ON,VID_TIME,VID_WEB_MODE)
-    print(info2)
+    # print(info2)
     pcam.info1 = info1
     pcam.info2 = info2
     try:
