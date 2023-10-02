9dc6727c48b3f640c3e39f3cab0a1eedb556dfc7
tsergiosoft <sergtarasenko76@gmail.com>
2023-10-02 00:02:10 -0700

    open cv

--- install.sh
+++ install.sh
@@ -14,56 +14,39 @@ sudo cp $HOME/awmcam/service/usb-mount@.service /etc/systemd/system/
 sudo cp $HOME/awmcam/service/99-local.rules /etc/udev/rules.d/
 sudo udevadm control --reload-rules
 sudo systemctl daemon-reload
-
 ####journalctl -xe
 
-#echo "----------SSH copy"
-#mkdir $HOME/.ssh
-#mkdir $HOME/mavlogs
-#cp -R $HOME/awmcam/ssh/* $HOME/.ssh
-#sudo chmod -R 400 $HOME/.ssh
-#sudo chmod 755 $HOME/.ssh
-#chmod 600 $HOME/.ssh/known_hosts
-#sudo ssh-copy-id -i ~/.ssh/tunkey.pub pi@127.0.0.1
-#echo "----------apt update"
-#sudo apt update
-#sudo apt upgrade
-#echo "----------Install netcat"
-#sudo sudo apt-get install ncat -y
-#echo "----------Install screen"
-#sudo apt-get install screen -y
-#echo "----------Remove modemmanager"
-#sudo apt-get remove modemmanager -y
-#echo "----------Install pip3"
-#sudo apt-get install python3-pip -y
-#echo "----------Install OpenCV"
-#sudo apt-get install build-essential cmake pkg-config libjpeg-dev libtiff5-dev libjasper-dev libpng-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libfontconfig1-dev libcairo2-dev libgdk-pixbuf2.0-dev libpango1.0-dev libgtk2.0-dev libgtk-3-dev libatlas-base-dev gfortran libhdf5-dev libhdf5-serial-dev libhdf5-103 python3-pyqt5 python3-dev -y
-#sudo apt install python3-opencv
-#pip install -U numpy
+echo "----------SSH copy"
+mkdir $HOME/.ssh
+mkdir $HOME/mavlogs
+cp -R $HOME/awmcam/ssh/* $HOME/.ssh
+sudo chmod -R 400 $HOME/.ssh
+sudo chmod 755 $HOME/.ssh
+chmod 600 $HOME/.ssh/known_hosts
+sudo ssh-copy-id -i ~/.ssh/tunkey.pub pi@127.0.0.1
+echo "----------apt update"
+sudo apt update
+sudo apt upgrade
+echo "----------Install netcat"
+sudo sudo apt-get install ncat -y
+echo "----------Install screen"
+sudo apt-get install screen -y
+echo "----------Remove modemmanager"
+sudo apt-get remove modemmanager -y
+echo "----------Install pip3"
+sudo apt-get install python3-pip -y
+echo "----------Install OpenCV"
+sudo apt-get install build-essential cmake pkg-config libjpeg-dev libtiff5-dev libjasper-dev libpng-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libfontconfig1-dev libcairo2-dev libgdk-pixbuf2.0-dev libpango1.0-dev libgtk2.0-dev libgtk-3-dev libatlas-base-dev gfortran libhdf5-dev libhdf5-serial-dev libhdf5-103 python3-pyqt5 python3-dev -y
+sudo apt install python3-opencv
+pip install -U numpy
 
-#echo "----------Install MAVProxy"
-#sudo pip install MAVProxy
-#echo "----------Install dronekit"
-#sudo pip install dronekit
-#echo "----------Install ZeroTier"
-#curl -s https://install.zerotier.com | sudo bash
-#sudo zerotier-cli join 1d71939404a9b1e4
-
-
-#echo "--------Install ffmpeg"
-#sudo apt-get install ffmpeg
-
-#cd $HOME
-#git clone https://github.com/jacksonliam/mjpg-streamer.git
-#git clone https://github.com/tsergiosoft/mjpg-streamer.git
-#sudo apt-get install cmake -y
-##sudo apt-get install libjpeg8-dev -y
-#sudo apt-get install libjpeg62-turbo-dev -y
-#sudo apt-get install gcc g++ -y
-#sudo apt-get install cmake
-#cd $HOME/mjpg-streamer/mjpg-streamer-experimental
-#make
-#sudo make install
+echo "----------Install MAVProxy"
+sudo pip install MAVProxy
+echo "----------Install dronekit"
+sudo pip install dronekit
+echo "----------Install ZeroTier"
+curl -s https://install.zerotier.com | sudo bash
+sudo zerotier-cli join 1d71939404a9b1e4
 
 echo "----------Create service mav"
 sudo cp $HOME/awmcam/service/awm.service /etc/systemd/system
@@ -82,8 +65,20 @@ echo ................... sudo ssh -v -i ~/.ssh/tunaws.pem ubuntu@13.50.210.14 -p
 #AllowTcpForwarding yes
 #GatewayPorts yes
 
-# Mission Planner -> Video->SetMJPEG source -> http://13.50.210.14:5000/?action=stream
-#
+
+#cd $HOME
+#git clone https://github.com/jacksonliam/mjpg-streamer.git
+#git clone https://github.com/tsergiosoft/mjpg-streamer.git
+#sudo apt-get install cmake -y
+##sudo apt-get install libjpeg8-dev -y
+#sudo apt-get install libjpeg62-turbo-dev -y
+#sudo apt-get install gcc g++ -y
+#sudo apt-get install cmake
+#cd $HOME/mjpg-streamer/mjpg-streamer-experimental
+#make
+#sudo make install
+### Mission Planner -> Video->SetMJPEG source -> http://13.50.210.14:5000/?action=stream
+
 # Connect from any to Raspi via SSh
 # USER --pi---!!!!!
 # ssh pi@13.50.210.14 -v -i C:\Users\Tarasenko_S\.ssh\tunaws.pem -p 5022
@@ -100,7 +95,7 @@ echo ................... sudo ssh -v -i ~/.ssh/tunaws.pem ubuntu@13.50.210.14 -p
 
 #/boot/config.txt entries to disable both Bluetooth and WiFi.
 #dtoverlay=disable-bt
-#dtoverlay=imx477 ##FOR HQ Camera 
+#??????dtoverlay=imx477 ##FOR HQ Camera
 
 ################################
 #on GitHub create repo tsergiosoft/arp.git
@@ -115,8 +110,6 @@ echo ................... sudo ssh -v -i ~/.ssh/tunaws.pem ubuntu@13.50.210.14 -p
 
 #sudo nano /etc/wpa_supplicant/wpa_supplicant.conf
 
-
-#sudo nano ./wpa_supplicant/wpa_supplicant.conf 
 #ON Cygwin ./tools/autotest/sim_vehicle.py -v ArduPlane -N -L KSFO --map --console --out 192.168.14.225:14550
 #-------------------------
 #put line to /etc/rc.local for autorun: 
