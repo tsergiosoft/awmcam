1032a04caa4f1215f1619cca76b2e8915348c75d
tsergiosoft <sergtarasenko76@gmail.com>
2023-10-02 12:14:53 -0700

    open cv

--- install.sh
+++ install.sh
@@ -21,48 +21,48 @@ sudo apt-get autoremove -y
 
 sudo cp $HOME/awmcam/service/config.txt /boot/
 
-#echo "----------SSH copy"
-#mkdir $HOME/.ssh
-#mkdir $HOME/mavlogs
-#cp -R $HOME/awmcam/ssh/* $HOME/.ssh
-#sudo chmod -R 400 $HOME/.ssh
-#sudo chmod 755 $HOME/.ssh
-#chmod 600 $HOME/.ssh/known_hosts
-#sudo ssh-copy-id -i ~/.ssh/tunkey.pub pi@127.0.0.1
-#echo "----------apt update"
-#sudo apt update
-#sudo apt upgrade
-#echo "----------Install netcat"
-#sudo sudo apt-get install ncat -y
-#echo "----------Install screen"
-#sudo apt-get install screen -y
-#echo "----------Remove modemmanager"
-#sudo apt-get remove modemmanager -y
-#echo "----------Install pip3"
-#sudo apt-get install python3-pip -y
-#echo "----------Install OpenCV"
-#sudo apt-get install build-essential cmake pkg-config libjpeg-dev libtiff5-dev libjasper-dev libpng-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libfontconfig1-dev libcairo2-dev libgdk-pixbuf2.0-dev libpango1.0-dev libgtk2.0-dev libgtk-3-dev libatlas-base-dev gfortran libhdf5-dev libhdf5-serial-dev libhdf5-103 python3-pyqt5 python3-dev -y
-#sudo apt install python3-opencv
-#pip install -U numpy
-#
-#echo "----------Install MAVProxy"
-#sudo pip install MAVProxy
-#echo "----------Install dronekit"
-#sudo pip install dronekit
-#echo "----------Install ZeroTier"
-#curl -s https://install.zerotier.com | sudo bash
-#sudo zerotier-cli join 1d71939404a9b1e4
-#
-#echo "----------Create service mav"
-#sudo cp $HOME/awmcam/service/awm.service /etc/systemd/system
-#sudo systemctl enable awm.service
-#echo "----------Start service mav"
-#sudo systemctl start awm.service
-#echo "----------Install finish OK"
-#echo -------------First ssh connect to cloud...
-#echo "--------------------------------------------END----------------------------------------------"
-#echo ................... execute test connect:
-#echo ................... sudo ssh -v -i ~/.ssh/tunaws.pem ubuntu@13.50.210.14 -p 22
+echo "----------SSH copy"
+mkdir $HOME/.ssh
+mkdir $HOME/mavlogs
+cp -R $HOME/awmcam/ssh/* $HOME/.ssh
+sudo chmod -R 400 $HOME/.ssh
+sudo chmod 755 $HOME/.ssh
+chmod 600 $HOME/.ssh/known_hosts
+sudo ssh-copy-id -i ~/.ssh/tunkey.pub pi@127.0.0.1
+echo "----------apt update"
+sudo apt update
+sudo apt upgrade
+echo "----------Install netcat"
+sudo sudo apt-get install ncat -y
+echo "----------Install screen"
+sudo apt-get install screen -y
+echo "----------Remove modemmanager"
+sudo apt-get remove modemmanager -y
+echo "----------Install pip3"
+sudo apt-get install python3-pip -y
+echo "----------Install OpenCV"
+sudo apt-get install build-essential cmake pkg-config libjpeg-dev libtiff5-dev libjasper-dev libpng-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libfontconfig1-dev libcairo2-dev libgdk-pixbuf2.0-dev libpango1.0-dev libgtk2.0-dev libgtk-3-dev libatlas-base-dev gfortran libhdf5-dev libhdf5-serial-dev libhdf5-103 python3-pyqt5 python3-dev -y
+sudo apt install python3-opencv
+pip install -U numpy
+
+echo "----------Install MAVProxy"
+sudo pip install MAVProxy
+echo "----------Install dronekit"
+sudo pip install dronekit
+echo "----------Install ZeroTier"
+curl -s https://install.zerotier.com | sudo bash
+sudo zerotier-cli join 1d71939404a9b1e4
+
+echo "----------Create service mav"
+sudo cp $HOME/awmcam/service/awm.service /etc/systemd/system
+sudo systemctl enable awm.service
+echo "----------Start service mav"
+sudo systemctl start awm.service
+echo "----------Install finish OK"
+echo -------------First ssh connect to cloud...
+echo "--------------------------------------------END----------------------------------------------"
+echo ................... execute test connect:
+echo ................... sudo ssh -v -i ~/.ssh/tunaws.pem ubuntu@13.50.210.14 -p 22
 
 #on AWS\Google ->
 # sudo nano /etc/ssh/sshd_config
